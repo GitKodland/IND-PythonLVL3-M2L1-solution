1a7c4c191059b6e2f5b0a91e0a6618bbc941c3a3
GitKodland <125306926+GitKodland@users.noreply.github.com>
2025-02-16 15:03:29 +0700

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,74 +1,68 @@
+import aiohttp
+import random
 from random import randint
-import requests
 
 class Pokemon:
-    pokemons = {} # { username : pokemon}
-    # Inisialisasi objek (konstruktor)
+    pokemons = {}
+
     def __init__(self, pokemon_trainer):
+        self.pokemon_trainer = pokemon_trainer
+        self.pokemon_number = random.randint(1, 1000)
+        self.name = None
+        self.img = None
+        self.power = random.randint(30, 60)
+        self.hp = random.randint(200, 400)
+        if pokemon_trainer not in self.pokemons:
+            self.pokemons[pokemon_trainer] = self
 
-        self.pokemon_trainer = pokemon_trainer   
-
-        self.pokemon_number = randint(1,1000)
-        self.img = self.get_img()
-        self.name = self.get_name()
-
-        self.power = randint(30, 60)
-        self.hp = randint(200, 400)
-
-        Pokemon.pokemons[pokemon_trainer] = self
-
-    # Metode untuk mendapatkan gambar pokemon melalui API
-    def get_img(self):
+    async def get_name(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            return (data['sprites']["other"]['official-artwork']["front_default"])
-        else:
-            return "https://static.wikia.nocookie.net/anime-characters-fight/images/7/77/Pikachu.png/revision/latest/scale-to-width-down/700?cb=20181021155144&path-prefix=ru"
-    
-    # Metode untuk mendapatkan nama pokemon melalui API
-    def get_name(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    return data['forms'][0]['name']
+                else:
+                    return "Pikachu"
+
+    async def info(self):
+        if not self.name:
+            self.name = await self.get_name()
+        return f"""Nama Pokemon kamu: {self.name}
+                Kekuatan Pokemon: {self.power}
+                HP Pokemon: {self.hp}"""
+
+    async def show_img(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            return (data['forms'][0]['name'])
-        else:
-            return "Pikachu"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    img_url = data['sprites']['front_default']
+                    return img_url 
+                else:
+                    return None
 
-
-    # Metode untuk mendapatkan informasi
-    def info(self):
-        return f"""Nama pokemon kamu: {self.name}
-Kekuatan pokemon: {self.power}
-Kesehatan pokemon: {self.hp}"""
-
-    # Metode untuk mendapatkan gambar pokemon
-    def show_img(self):
-        return self.img
-    
-    def attack(self, enemy):
+    async def attack(self, enemy):
         if isinstance(enemy, Wizard):
-            chance = randint(1,5)
+            chance = randint(1, 5)
             if chance == 1:
-                return "Pokemon-penyihir menggunakan perisai dalam pertarungan"
+                return "Pokemon Penyihir menggunakan perisai dalam pertarungan"
         if enemy.hp > self.power:
             enemy.hp -= self.power
-            return f"""Pertarungan antara @{self.pokemon_trainer} dan @{enemy.pokemon_trainer}
-Kesehatan @{enemy.pokemon_trainer} sekarang {enemy.hp}"""
+            return f"Pertarungan @{self.pokemon_trainer} melawan @{enemy.pokemon_trainer}\nHP @{enemy.pokemon_trainer} sekarang {enemy.hp}"
         else:
             enemy.hp = 0
-            return f"Kemenangan untuk @{self.pokemon_trainer} melawan @{enemy.pokemon_trainer}! "
+            return f"@{self.pokemon_trainer} menang melawan @{enemy.pokemon_trainer}!"
 
 class Wizard(Pokemon):
-  pass
-
+    # Kelas ini dapat menambahkan method dan properti khusus untuk penyihir
+    pass
 
 class Fighter(Pokemon):
-    def attack(self, enemy):
-        super_power = randint(5,15)
+    async def attack(self, enemy):
+        super_power = randint(5, 15)
         self.power += super_power
-        result = super().attack(enemy)
+        result = await super().attack(enemy)
         self.power -= super_power
-        return result + f"\nPetarung menggunakan serangan super dengan kekuatan: {super_power} "
+        return result + f"\nPetarung menggunakan serangan super dengan kekuatan:{super_power}"
